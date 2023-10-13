9ade04d735eecc87810f8d3e23146356ed6a9d29
Andrii Zhukov <262701@student.pwr.edu.pl>
2023-10-14 01:06:06 +0200

    Setting up Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import {SwaggerModule, DocumentBuilder} from "@nestjs/swagger";
 import { AppModule } from './app.module';
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+      .setTitle('pwr-api')
+      .setVersion('1.0')
+      .addServer('http://localhost:3000', 'Local environment')
+      .build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api-docs', app, document)
+
+  await app.listen(process.env.PORT || 3000)
 }
 bootstrap();
